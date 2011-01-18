995fb45ed6c6566760e09b1462bb7363036ffc64
alts <stephen@evilrobotstuff.com>
2011-01-17 22:26:32 -0800

    Forbids touchdown events from firing if a touchMove event occurred. Not ideal, but works for now

--- src/touchdown.js
+++ src/touchdown.js
@@ -1,12 +1,26 @@
 (function(window) {
+	// constants
+	var EVENT_WINDOW = 500,
+		TOUCH_RANGE = 1;
+
 	var bound = {},
 		touches = [],
 		touchdown_on = false,
-		listen_to_moves = false;
+		listen_to_moves = false,
+		last_move = null;
 
 	function hasClass(node, class_name) {
 		return (' ' + node.className + ' ').replace(/[\n\r\t]/g, ' ')
 			.indexOf(class_name) > -1;
+	}
+
+	function withinSquareRange(p1, p2, range) {
+		if (!p2)
+			return true;
+		return false;
+		// this is behaving as I had hoped. Let's forbid moves for now.
+		return p2.screenX > p1.screenX - range && p2.screenX < p1.screenX + range &&
+			p2.screenY > p1.screenY - range && p2.screenY < p1.screenY + range;
 	}
 
 	window.touchdown = function(class_name, callback) {
@@ -41,7 +55,7 @@
 
 			document.body.addEventListener('touchmove', function(e) {
 				if (listen_to_moves) {
-
+					last_move = e.touches[0];
 				}
 			}, false);
 
@@ -54,7 +68,9 @@
 
 						if (hasClass(n, class_key) && event) {
 							listen_to_moves = false;
-							if (info.time && now < info.time + 500) {
+							if (info.time && now < info.time + EVENT_WINDOW
+								&& withinSquareRange(event.touches[0], last_move, TOUCH_RANGE)) {
+
 								for (var i = 0, l = info.callbacks.length; i < l; i++) {
 									info.callbacks[i]({
 										type: 'touchdown',
@@ -68,6 +84,7 @@
 
 							setTimeout(function(){
 								info.touch = null;
+								last_move = null;
 							}, 500);
 						}
 					}
